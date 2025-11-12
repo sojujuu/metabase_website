9332d8187f39380d61285518d1076910f93e9f3c
julius <OS20085X@cimbniaga.co.id>
2025-11-12 11:18:25 +0700

    fix: some changes

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => ({
   base: "/metabase_website/",
   server: {
     host: "::",
-    port: 8080,
+    port: 8081,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
